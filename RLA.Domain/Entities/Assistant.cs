00d313720c5d94ab9b8e97865ae2a831821f33bf
adham-hashem <adhamhashem2024@gmail.com>
2025-06-02 21:45:37 +0300

    Add Assistant Entity

---
+++ RLA.Domain/Entities/Assistant.cs
@@ -0,0 +1,21 @@
+using static System.Collections.Specialized.BitVector32;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ELP.Domain.Entities
+{
+    public class Assistant
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<Section> Sections { get; set; } = new List<Section>();
+    }
+
+}
