c1c7f8b5ac3eee4b8af72c78cf231b7bcbb2e1f3
adham-hashem <adhamhashem2024@gmail.com>
2025-06-02 20:00:06 +0300

    Add Term Entity

---
+++ RLA.Domain/Entities/Term.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELP.Domain.Entities
+{
+    public class Term
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty; // e.g., "Fall 2025"
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; } = false; // Indicates the current term
+
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+        public ICollection<Quiz> Quizzes { get; set; } = new List<Quiz>();
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
+    }
+}
