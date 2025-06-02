f2b3bad4ed319ffa731f7f951c641613ce83b1e1
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 01:27:17 +0300

    Add Section Entity

---
+++ RLA.Domain/Entities/Section.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELP.Domain.Entities
+{
+    public class Section
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required, MaxLength(20)]
+        public string DayOfWeek { get; set; } = string.Empty;
+
+        [Required, MaxLength(20)]
+        public string Time { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Location { get; set; } = string.Empty;
+
+        [Required]
+        public Guid CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public Course Course { get; set; } = null!;
+
+        [Required]
+        public Guid AssistantId { get; set; }
+
+        [ForeignKey(nameof(AssistantId))]
+        public Assistant Assistant { get; set; } = null!;
+
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
+    }
+}
