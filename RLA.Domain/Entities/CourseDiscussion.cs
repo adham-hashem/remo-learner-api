d6584db30d5172857a65000ef08a7590fe71c4ac
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 01:14:46 +0300

    Add Course Discussion Entity

---
+++ RLA.Domain/Entities/CourseDiscussion.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELP.Domain.Entities
+{
+    public class CourseDiscussion
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public Course Course { get; set; } = null!;
+
+        [Required]
+        public Guid ProfessorId { get; set; }
+
+        [ForeignKey(nameof(ProfessorId))]
+        public Professor Professor { get; set; } = null!;
+
+        [Required, MaxLength(1000)]
+        public string Message { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime PostedAt { get; set; } = DateTime.UtcNow;
+    }
+}
