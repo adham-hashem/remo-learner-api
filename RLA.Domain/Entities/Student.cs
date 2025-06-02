db2ddda0a39e7deb7f78fad29e6084f879a9c849
adham-hashem <adhamhashem2024@gmail.com>
2025-06-02 18:17:55 +0300

    Add Student Entity

---
+++ RLA.Domain/Entities/Student.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELP.Domain.Entities
+{
+    public class Student
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
+        public ICollection<QuizSubmission> QuizSubmissions { get; set; } = new List<QuizSubmission>();
+    }
+}
