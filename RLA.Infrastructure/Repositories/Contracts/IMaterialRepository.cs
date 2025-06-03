c0dc07101eaba6e5c0a1e1541c8cb00f39718063
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 07:23:11 +0300

    Add Material Interface

---
+++ RLA.Infrastructure/Repositories/Contracts/IMaterialRepository.cs
@@ -0,0 +1,16 @@
+using RLA.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RLA.Infrastructure.Repositories.Contracts
+{
+    public interface IMaterialRepository
+    {
+        Task<Material> GetByIdAsync(Guid id);
+        Task<IEnumerable<Material>> GetByCourseIdAsync(Guid courseId);
+        Task AddAsync(Material material);
+        Task UpdateAsync(Material material);
+        Task DeleteAsync(Guid id);
+    }
+}
