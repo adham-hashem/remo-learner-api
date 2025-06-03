b87fd3d0438b4c83ac26da6cc485489907960a94
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 03:19:12 +0300

    Add Answer Entity:

---
+++ RLA.Domain/Entities/Answer.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELP.Domain.Entities
+{
+    public class Answer
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Text { get; set; } = string.Empty;
+
+        [Required]
+        public bool IsCorrect { get; set; }
+
+        [Required]
+        public Guid QuestionId { get; set; }
+
+        [ForeignKey(nameof(QuestionId))]
+        public Question Question { get; set; } = null!;
+
+        public ICollection<QuizAnswer> QuizAnswers { get; set; } = new List<QuizAnswer>();
+    }
+}
