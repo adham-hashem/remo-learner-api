6a8bb2bc67338a00e008f68ffe611cd1301ac475
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 06:42:21 +0300

    Add Web Application Extensions

--- RLA.API/Extensions/WebApplicationExtensions.cs
+++ RLA.API/Extensions/WebApplicationExtensions.cs
@@ -1,6 +1,30 @@
-﻿namespace RLA.API.Extensions
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+
+namespace RLA.API.Extensions
 {
-    public class WebApplicationExtensions
+    public static class WebApplicationExtensions
     {
+        public static WebApplication ConfigureMiddleware(this WebApplication app)
+        {
+            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+            if (app.Environment.IsDevelopment())
+            {
+                logger.LogInformation("Running in Development mode");
+            }
+            else
+            {
+                logger.LogInformation("Running in Production mode");
+            }
+
+            app.UseCors("AllowAll");
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            return app;
+        }
     }
 }
