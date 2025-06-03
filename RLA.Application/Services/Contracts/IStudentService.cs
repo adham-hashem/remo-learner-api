0ae95fb4d2c429c40308c81acf62a065f8f69ada
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 22:22:32 +0300

    feat(services contracts): add student service contract

---
+++ RLA.Application/Services/Contracts/IStudentService.cs
@@ -0,0 +1,19 @@
+using RLA.Application.DTOs.UserDTOs;
+using RLA.Application.Entities;
+using RLA.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RLA.Application.Services.Contracts
+{
+    public interface IStudentService
+    {
+        Task<Student> CreateStudentAsync(AddStudentDto studentDto);
+        Task<Student> GetStudentByUserIdAsync(Guid userId);
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+        Task<IEnumerable<Student>> GetStudentsByCourseIdAsync(Guid courseId);
+        Task UpdateStudentAsync(Guid userId, UpdateUserDto updateDto);
+        Task DeleteStudentAsync(Guid userId);
+    }
+}
