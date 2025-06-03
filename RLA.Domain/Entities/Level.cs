9ea1a8b9146a009131f56de67b5fd64bca1b0841
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 03:02:37 +0300

    Add Level Entity

---
+++ RLA.Domain/Entities/Level.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ELP.Domain.Entities
+{
+    public class Level
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [Range(1, 4, ErrorMessage = "The level number must be a number from 1 to 4")]
+        public int Number { get; set; }
+
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+
+}
