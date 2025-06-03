83e80ab88c5984ed4eea4631c815d7060aad615a
adham-hashem <adhamhashem2024@gmail.com>
2025-06-03 22:16:25 +0300

    refactor(services contracts): change professor service contract namespace

--- RLA.Application/Services/Contracts/IProfessorService.cs
+++ RLA.Application/Services/Contracts/IProfessorService.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace ELP.Services.Contracts
+namespace RLA.Application.Services.Contracts
 {
     public interface IProfessorService
     {
