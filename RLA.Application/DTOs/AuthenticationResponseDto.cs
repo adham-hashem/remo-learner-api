4633f061a43db92c911bb8bd5483df92cbf721ef
adham-hashem <adhamhashem2024@gmail.com>
2025-06-10 23:30:33 +0300

    refactor(auth DTOs): initialize values of authentication response DTO properties

--- RLA.Application/DTOs/AuthenticationResponseDto.cs
+++ RLA.Application/DTOs/AuthenticationResponseDto.cs
@@ -5,10 +5,10 @@ namespace RLA.Application.DTOs
     /// </summary>
     public class AuthenticationResponseDto
     {
-        public string Token { get; set; }
-        public string UserId { get; set; }
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string Role { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
     }
 }
