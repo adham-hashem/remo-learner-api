da31a00b38bb191144927e0e15e9d8e4a6937b31
adham-hashem <adhamhashem2024@gmail.com>
2025-06-11 22:22:23 +0300

    feat(auth DTOs): add two factor setup DTO

---
+++ RLA.Application/DTOs/AuthDTOs/TwoFactorSetupDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLA.Application.DTOs.AuthDTOs
+{
+    public class TwoFactorSetupDto
+    {
+        public Guid UserId { get; set; }
+        public string SecretKey { get; set; } = string.Empty;
+        public string QrCodeUri { get; set; } = string.Empty;
+    }
+}
