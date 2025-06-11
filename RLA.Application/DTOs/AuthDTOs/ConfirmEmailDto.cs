b6dfc8cdd4ab0b72fcacc51be8877c99407fdbae
adham-hashem <adhamhashem2024@gmail.com>
2025-06-11 22:19:59 +0300

    feat(auth DTOs): add confirm email DTO

---
+++ RLA.Application/DTOs/AuthDTOs/ConfirmEmailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLA.Application.DTOs.AuthDTOs
+{
+    public class ConfirmEmailDto
+    {
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+    }
+}
