50a9f1206a81de7772e45c76be841b9ef4b1cd26
adham-hashem <adhamhashem2024@gmail.com>
2025-06-12 03:29:34 +0300

    feat(services contracts): add authentication service contract

---
+++ RLA.Application/Services/Contracts/IAuthService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLA.Application.DTOs.AuthDTOs;
+using RLA.Application.DTOs;
+using RLA.Domain.Entities;
+
+namespace RLA.Application.Services.Contracts
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> LoginAsync(LoginRequestDTO loginDto);
+        Task<ApplicationUser> RegisterStudentAsync(RegisterStudentDto registerDto);
+        Task<ApplicationUser> RegisterProfessorAsync(RegisterProfessorDto registerDto);
+        Task<AuthResponseDto> GenerateAuthResponseAsync(ApplicationUser user, IEnumerable<string> roles);
+        Task ConfirmEmailAsync(Guid userId, string token);
+        Task<TwoFactorSetupDto> EnableTwoFactorAsync(Guid userId);
+        Task<AuthResponseDto> VerifyTwoFactorAsync(string email, string code);
+        Task<AuthResponseDto> ExternalLoginAsync(ExternalLoginDto externalDto);
+        Task ForgotPasswordAsync(string email);
+        Task ResetPasswordAsync(ResetPasswordDto resetDto);
+        Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+        Task LogoutAsync(Guid userId);
+    }
+}
