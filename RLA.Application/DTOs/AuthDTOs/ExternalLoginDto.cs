a1f16d9b19840dfed20eb1c2f6cb87e72644ff7b
adham-hashem <adhamhashem2024@gmail.com>
2025-06-12 03:07:30 +0300

    feat(auth DTOs): add external login DTO

---
+++ RLA.Application/DTOs/AuthDTOs/ExternalLoginDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLA.Application.DTOs.AuthDTOs
+{
+    public class ExternalLoginDto
+    {
+        [Required]
+        public string Provider { get; set; } = string.Empty;
+
+        [Required]
+        public string ProviderKey { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+    }
+}
